9741ae5335a8c138d99df62bb1af856bbdcf02ba
Blake Kaufman <blakekaufman9@gmail.com>
2025-01-22 17:23:21 -0500

    adding constants

---
+++ app/constants/icons.js
@@ -0,0 +1,4 @@
+import leftArrow from '../assets/leftArrow.png';
+import lockIcon from '../assets/lockIcon.png';
+
+export default {leftArrow, lockIcon};
---
+++ app/constants/index.js
@@ -0,0 +1,28 @@
+const NETWORK_ENVIRONMENT = 'testnet';
+const BACKGROUND_COLOR = '#F9FFFD';
+const BACKGROUND_OFFSET_COLOR = '#EBF3F0';
+const ALTERNATE_TEXT = '#247BA0';
+const TEXT_COLOR = '#2C2C2C';
+const WINDOW_WIDTH = '90%';
+const CENTER_ELEMENT = {
+  marginRight: 'auto',
+  marginLeft: 'auto',
+};
+import ICONS from './icons';
+
+const BACK_ARROW = {
+  width: 30,
+  height: 30,
+};
+
+export {
+  NETWORK_ENVIRONMENT,
+  BACKGROUND_COLOR,
+  BACKGROUND_OFFSET_COLOR,
+  TEXT_COLOR,
+  WINDOW_WIDTH,
+  CENTER_ELEMENT,
+  ICONS,
+  BACK_ARROW,
+  ALTERNATE_TEXT,
+};
